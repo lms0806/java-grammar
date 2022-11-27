6232158835442f02266b2e10014ba88be7f6a55a
lms0806 <godmlzkf1@naver.com>
2022-11-27 20:17:43 +0900

    [UPD] BigInteger

--- BigInteger/BigInteger.java
+++ BigInteger/BigInteger.java
@@ -1,23 +1,31 @@
 import java.math.BigInteger;
 
 public class Main {
-	public BigInteger int_to_BigInteger(int n) {
+	public static BigInteger int_to_BigInteger(int n) {
 		return BigInteger.valueOf(n);
 	}
 	
-	public BigInteger add(int a, int b) {
+	public static BigInteger String_to_BigInteger(String s) {
+		return new BigInteger(s);
+	}
+	
+	public static String ten_to_sixteen(BigInteger n) {
+		return n.toString(16);
+	}
+	
+	public static BigInteger add(int a, int b) {
 		return int_to_BigInteger(a).add(int_to_BigInteger(b));
 	}
 	
-	public BigInteger minus(int a, int b) {
+	public static BigInteger minus(int a, int b) {
 		return int_to_BigInteger(a).min(int_to_BigInteger(b));
 	}
 	
-	public BigInteger mul(int a, int b) {
+	public static BigInteger mul(int a, int b) {
 		return int_to_BigInteger(a).multiply(int_to_BigInteger(b));
 	}
 	
-	public BigInteger divide(int a, int b) {
+	public static BigInteger divide(int a, int b) {
 		return int_to_BigInteger(a).divide(int_to_BigInteger(b));
 	}
 }
